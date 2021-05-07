55a1f8f80d2df4e946dea38c4a269349125f56ec
HoustonOla35 <zzeroexplorer@gmail.com>
2021-05-07 18:57:17 +0200

    Update stats

--- spheres/main.cpp
+++ spheres/main.cpp
@@ -4,7 +4,6 @@
 #include <QLabel>
 
 #include <iostream>
-#include <iomanip>
 
 #include "renderer/Stats.hpp"
 #include "renderer/Scene.hpp"
@@ -34,33 +33,11 @@ int main (int argc, char *argv[])
     QApplication app(argc, argv);
 
     Stats::init();
-    const clock_t startTime = clock();
 
     Image *image = generateImage();
 
-    const clock_t endTime = clock();
-
-    // Show stats
-    const int width1 = 40;
-    const int width2 = 16;
-
-    std::cout.imbue(std::locale("")); // Pretty print numbers
-    std::cout << "##### Statistics\n";
-
-    std::cout << std::left  << std::setw(width1) << "Total CPU time:"
-              << std::right << std::setw(width2-2) << (double)(endTime-startTime) / CLOCKS_PER_SEC << " s\n";
-
-    std::cout << std::left  << std::setw(width1) << "Ray-triangle tests count:"
-              << std::right << std::setw(width2) << Stats::rayTriangleTestsCount() << "\n";
-    std::cout << std::left  << std::setw(width1) << "Ray-triangle intersections count:"
-              << std::right << std::setw(width2) << Stats::rayTriangleIntersectionsCount() << "\n";
-
-    std::cout << std::left  << std::setw(width1) << "Ray-box tests count:"
-              << std::right << std::setw(width2) << Stats::rayBoxTestsCount() << "\n";
-    std::cout << std::left  << std::setw(width1) << "Ray-box intersections count:"
-              << std::right << std::setw(width2) << Stats::rayBoxIntersectionsCount() << "\n";
-
-    std::cout.flush();
+    Stats::endTimer();
+    Stats::print(std::cout);
 
     showImage(*image);
 
